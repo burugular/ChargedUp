673bba8c9b86f1a2e9ae5900959764914d6728f5
Burugula Rao <burugularao@gmail.com>
2019-05-19 23:59:32 +0100

    no message

--- app/src/main/java/com/rao/com/googlemapschargedup/directionHelpers/PointsParser.java
+++ app/src/main/java/com/rao/com/googlemapschargedup/directionHelpers/PointsParser.java
@@ -4,12 +4,9 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.util.Log;
-
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
-
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
